a7df8b1b2ad9000c0541f9acbbe44a043e660359
matsuryuji <mrmatsutane@hotmail.com>
2022-10-28 23:44:07 -0300

    first commit

---
+++ models/model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const dataSchema = new mongoose.Schema({
+  name: {
+    required: true,
+    type: String,
+  },
+  lastName: {
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    type: String,
+  },
+  phone: {
+    required: true,
+    type: String,
+  },
+  age: {
+    required: true,
+    type: Number,
+  },
+});
+
+module.exports = mongoose.model("Data", dataSchema);
